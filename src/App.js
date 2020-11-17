115a7bf2d31c905f0395d1d85e6c6ed061efe867
matthew-howe <mattehowe@gmail.com>
2020-11-17 01:35:06 -0500

    adds navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import RPSSolverInterface from './rps';
+import Navbar from './components/navbar';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <RPSSolverInterface />
     </div>
   );
