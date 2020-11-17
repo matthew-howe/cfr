f74231cfcc3d3a0d92419a9e62f0ccd842863ab5
matthew-howe <mattehowe@gmail.com>
2020-11-17 01:15:28 -0500

    rps solver mvp

--- src/rps/components/villStratForm.js
+++ src/rps/components/villStratForm.js
@@ -1,36 +1,41 @@
 import React from 'react';
 
-export default function VillainStratForm({ strat }) {
-  
+export default function VillainStratForm({strat}) {
+  function handleSubmit() {}
 
-  function handleSubmit() {
-
-  }
-  
-  function handleChange(evt) {
-     
-  }
+  function handleChange(evt) {}
   return (
     <div className="rps-form">
-       <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <label>
-          Rock
-          <input type="text" value={strat[0]} onChange={handleChange} />
+          <input
+            type="text"
+            id="rfreq"
+            value={strat[0].toFixed(2)}
+            onChange={handleChange}
+          />
         </label>
       </form>
-       <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <label>
-          Paper
-          <input type="text" value={strat[1]} onChange={handleChange} />
+          <input
+            type="text"
+            id="pfreq"
+            value={strat[1].toFixed(2)}
+            onChange={handleChange}
+          />
         </label>
       </form>
-       <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <label>
-          Scissors
-          <input type="text" value={strat[2]} onChange={handleChange} />
+          <input
+            type="text"
+            id="sfreq"
+            value={strat[2].toFixed(2)}
+            onChange={handleChange}
+          />
         </label>
       </form>
-      </div>
-  )
+    </div>
+  );
 }
-
--- src/rps/index.js
+++ src/rps/index.js
@@ -5,26 +5,65 @@ import VillainStratForm from './components/villStratForm';
 import RegretVisualizer from './components/regretVisualizer';
 import RPSSolver from './RPSSolverSelfTrain';
 
+import './rps.css';
+
 let _go = true;
 let _regrets = [0, 0, 0]
 
 function RPSSolverInterface({ updateRegrets }) {
   let [villStrat, setVillStrat] = useState([.3333333, .3333333, .333333])
   let [regretsHistory, setRegretsHistory] = useState([]);
+  let [iterations, setIterations] = useState(1000);
+  let [shouldRun, setShouldRun] = useState(false);
+  let [monkey, rerender] = useState(false);
+  
   let i = 0;
   let regretMap = {};
   const setRegretSum = regrets => {
     i++;
     updateRegrets(regrets);
   }
-  let solver = new RPSSolver(villStrat, setRegretSum);
-  solver.train(100000);
-  console.log('zzz', solver.getAvgStrategy());
+  let avgStrat = [0,0,0]
 
+  if (shouldRun) {
+    let solver = new RPSSolver(villStrat, setRegretSum);
+    solver.train(iterations);
+    avgStrat = solver.getAvgStrategy();
+  }
+
+  const handleChange = evt => {
+    setIterations(evt.target.value);
+  }
+
+  const runSolver = () => {
+    if (shouldRun === false) {
+      setShouldRun(true);
+    } else {
+      rerender(!monkey); 
+    }
+  }
 
   return (
     <div>
-      <VillainStratForm strat={villStrat} setStrat={setVillStrat} />
+      <div className="controls">
+        <button id="rps-run-btn" onClick={() => runSolver()}>
+          RUN
+          </button>
+        <div id="rps-iterations">
+          <div>ITERATIONS</div>
+      <form >
+        <label>
+          <input
+            type="text"
+            id=""
+            value={iterations}
+            onChange={handleChange}
+          />
+        </label>
+      </form>
+          </div>
+      <VillainStratForm strat={avgStrat} setStrat={setVillStrat} />
+        </div>
       <RegretVisualizer regrets={regretMap} />
     </div>
   )
